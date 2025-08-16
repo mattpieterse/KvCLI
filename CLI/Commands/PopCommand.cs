d72d65a8b9518dc71bc9e09a38c3aea5c4db2d0a
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-08-16 18:44:47 +0200

    feat(command): create scaffolding for the pop command

---
+++ CLI/Commands/PopCommand.cs
@@ -0,0 +1,19 @@
+﻿using Spectre.Console.Cli;
+
+namespace CLI.Commands;
+
+internal sealed class PopCommand : Command<PopCommand.Settings>
+{
+#region Settings
+
+    internal abstract class Settings : CommandSettings { }
+
+#endregion
+
+#region Function
+
+    public override int Execute(CommandContext context, Settings settings)
+        => throw new NotImplementedException();
+
+#endregion
+}
