72297532a9af7bf19c377355aad9d7fe0e842299
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-08-17 01:32:22 +0200

    feat: create key-value serializers for storing json configurations

---
+++ CLI/Services/Serialization/JsonKeyValueSerializer.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace CLI.Services.Serialization;
+
+public class JsonKeyValueSerializer : IKeyValueSerializer
+{
+#region Functions
+
+    public string Serialize(Dictionary<string, string> secrets) {
+        return JsonSerializer.Serialize(
+            secrets, new JsonSerializerOptions {
+                WriteIndented = true
+            }
+        );
+    }
+
+
+    public Dictionary<string, string> Deserialize(string oJson) {
+        return JsonSerializer.Deserialize<Dictionary<string, string>>(oJson)
+               ?? new Dictionary<string, string>();
+    }
+
+#endregion
+}
