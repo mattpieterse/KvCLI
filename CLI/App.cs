b0116894635af66434c4cf02c40a094d7f0ec596
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-08-16 17:55:13 +0200

    feat: create the initial project scaffolding
    resolves #3

---
+++ CLI/App.cs
@@ -0,0 +1,13 @@
+﻿namespace CLI;
+
+/// <summary>
+/// The main entry-point for the CLI application.
+/// </summary>
+internal abstract class App
+{
+#region Lifecycle
+
+    private static void Main(string[] args) { }
+
+#endregion
+}
