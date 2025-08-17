c9bf862dd0e3ac8d4af03926167a8c7fdb8b063b
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-08-17 02:13:02 +0200

    feat(command): add implementation for the get command

--- CLI/Commands/GetCommand.cs
+++ CLI/Commands/GetCommand.cs
@@ -1,21 +1,38 @@
-﻿using JetBrains.Annotations;
+﻿using CLI.Services.Store;
+using JetBrains.Annotations;
+using Spectre.Console;
 using Spectre.Console.Cli;
 
 namespace CLI.Commands;
 
-internal sealed class GetCommand : Command<GetCommand.Settings>
+[UsedImplicitly]
+internal sealed class GetCommand(
+    ValueStore kvStore
+) : Command<GetCommand.Settings>
 {
 #region Settings
 
     [UsedImplicitly]
-    internal sealed class Settings : CommandSettings { }
+    internal sealed class Settings : CommandSettings
+    {
+        [CommandArgument(0, "<KEY>")]
+        public string Key { get; set; } = string.Empty;
+    }
 
 #endregion
 
 #region Function
 
-    public override int Execute(CommandContext context, Settings settings)
-        => throw new NotImplementedException();
+    public override int Execute(
+        CommandContext context, Settings settings
+    ) {
+        var value = kvStore.FetchOne(settings.Key);
+        AnsiConsole.MarkupLineInterpolated((value == null)
+            ? (FormattableString) $"[red]Could not find a value associated with [{settings.Key}][/]"
+            : (FormattableString) $"[green]FetchOne >> {settings.Key}=[{value}][/]");
+
+        return 0;
+    }
 
 #endregion
 }
