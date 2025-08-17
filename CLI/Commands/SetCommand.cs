b05b057c89318b93290e775cf7336208549cf9ed
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-08-17 02:12:49 +0200

    feat(command): add implementation for the set command

--- CLI/Commands/SetCommand.cs
+++ CLI/Commands/SetCommand.cs
@@ -1,21 +1,39 @@
-﻿using JetBrains.Annotations;
+﻿using CLI.Services.Store;
+using JetBrains.Annotations;
+using Spectre.Console;
 using Spectre.Console.Cli;
 
 namespace CLI.Commands;
 
-internal sealed class SetCommand : Command<SetCommand.Settings>
+[UsedImplicitly]
+internal sealed class SetCommand(
+    ValueStore kvStore
+) : Command<SetCommand.Settings>
 {
 #region Settings
 
     [UsedImplicitly]
-    internal sealed class Settings : CommandSettings { }
+    internal sealed class Settings : CommandSettings
+    {
+        [CommandArgument(0, "<KEY>")]
+        public string Key { get; set; } = string.Empty;
+
+
+        [CommandArgument(1, "<VALUE>")]
+        public string Value { get; set; } = string.Empty;
+    }
 
 #endregion
 
 #region Function
 
-    public override int Execute(CommandContext context, Settings settings)
-        => throw new NotImplementedException();
+    public override int Execute(
+        CommandContext context, Settings settings
+    ) {
+        kvStore.Upsert(settings.Key, settings.Value);
+        AnsiConsole.MarkupLineInterpolated($"[green]Upsert >> {settings.Key}=[{settings.Value}][/]");
+        return 0;
+    }
 
 #endregion
 }
